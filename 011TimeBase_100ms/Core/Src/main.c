f34391beb31f7e63505c8633cc6017d7d2690df7
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2026-08-04 18:04:50 +0300

    Implement toggle LED when timer ticks (manual)

--- 011TimeBase_100ms/Core/Src/main.c
+++ 011TimeBase_100ms/Core/Src/main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 void SystemClock_Config(void);
+void GPIO_Init(void);
 void TIM11_Init(void);
 void Error_Handler(void);
 
@@ -9,9 +10,21 @@ TIM_HandleTypeDef htimer11;
 int main(void) {
 	HAL_Init();
 	SystemClock_Config();
+	GPIO_Init();
 	TIM11_Init();
 
-	while (1);
+	// Let's start timer
+	HAL_TIM_Base_Start(&htimer11);
+
+	while (1) {
+		// Loop until the update event flag is set
+		while (!(TIM11->SR & TIM_SR_UIF));
+
+		/* The required time delay has been elapsed */
+		/* User code can be executed */
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+		TIM11->SR |= 0 << 1;
+	}
 
 	return 0;
 }
@@ -52,17 +65,27 @@ void SystemClock_Config(void) {
 	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
 }
 
+void GPIO_Init(void) {
+	GPIO_InitTypeDef ledgpio;
+
+	ledgpio.Pin = GPIO_PIN_13;
+	ledgpio.Mode = GPIO_MODE_OUTPUT_PP;
+	ledgpio.Pull = GPIO_NOPULL;
+
+	HAL_GPIO_Init(GPIOC, &ledgpio);
+}
+
 void TIM11_Init(void) {
 	htimer11.Instance = TIM11;
-	// CounterMode is always default for Basic Timers and cannot be changed
+// CounterMode is always default for Basic Timers and cannot be changed
 	htimer11.Init.CounterMode = TIM_COUNTERMODE_UP;
-	// CNT_CLK = TIMxCLK from RCC
-	// CNT_CLK = TIMx_CLK / (prescaler + 1)
-	// HSE CNT_CLK = 25 / (1 + 1) = 12
-	// Range 0x00 to 0xFFFF (16bit)
+// CNT_CLK = TIMxCLK from RCC
+// CNT_CLK = TIMx_CLK / (prescaler + 1)
+// HSE CNT_CLK = 25 / (1 + 1) = 12
+// Range 0x00 to 0xFFFF (16bit)
 	htimer11.Init.Prescaler = 999;
-	// if value 0 timer won't start
-	// we need -1 to get the exact count as it will take +1 more
+// if value 0 timer won't start
+// we need -1 to get the exact count as it will take +1 more
 	htimer11.Init.Period = 10000 - 1;
 
 	if (HAL_TIM_Base_Init(&htimer11) != HAL_OK) {
