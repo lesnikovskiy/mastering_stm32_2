d9c85d2b1e30671ecf24b8a1417bd86c834e1844
Ruslan Lesnikovskiy <Ruslan.Lesnikovskiy@materialise.kiev.ua>
2026-08-10 16:38:07 +0300

    Add project for 10 micro seconds

---
+++ 018TimeBase_HSI_Micro10/Core/Src/main.c
@@ -0,0 +1,64 @@
+#include "main.h"
+
+void SystemClock_Config(void);
+void GPIO_Init(void);
+void TIM11_Init(void);
+void Error_Handler(void);
+
+TIM_HandleTypeDef htimer11;
+
+int main(void) {
+	HAL_Init();
+
+	SystemClock_Config();
+
+	GPIO_Init();
+	TIM11_Init();
+
+	// Let's start the timer
+	HAL_TIM_Base_Start_IT(&htimer11);
+
+	while (1);
+
+	return 0;
+}
+
+void SystemClock_Config(void) {
+	// Let's do it later and use HSI 16Mhz by default
+}
+
+void GPIO_Init(void) {
+	__HAL_RCC_GPIOC_CLK_ENABLE();
+
+	GPIO_InitTypeDef ledgpio;
+	ledgpio.Pin = GPIO_PIN_13;
+	ledgpio.Mode = GPIO_MODE_OUTPUT_PP;
+	ledgpio.Pull = GPIO_NOPULL;
+
+	HAL_GPIO_Init(GPIOC, &ledgpio);
+}
+
+void TIM11_Init(void) {
+	htimer11.Instance = TIM11;
+
+	htimer11.Init.CounterMode = TIM_COUNTERMODE_UP;
+	htimer11.Init.Prescaler = 24;
+	htimer11.Init.Period = 64000 - 1;
+
+	if (HAL_TIM_Base_Init(&htimer11) != HAL_OK) {
+		Error_Handler();
+	}
+}
+
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+	if (htim->Instance == TIM11) {
+		// Toggle pin 13
+		GPIOC->ODR ^= GPIO_PIN_13;
+	}
+}
+
+void Error_Handler(void) {
+	__disable_irq();
+
+	while (1);
+}
